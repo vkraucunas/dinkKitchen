c549242cf70a283c86affebd6317931f7f939d14
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-02-02 11:03:26 -0700

    finally got background + buttons

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,117 +1,149 @@
 $(document).ready(function() {
+
+//global variables
+  var $wholeThing = 'html, body';
+//choices object to be filled
+  var choices = {
+    meats: [],
+  }
   //step1
-  var $wholeThing = 'html, body'
+
   $('#adventure').on('click', function() {
     $($wholeThing).animate({
         scrollTop: $('#step6').offset().top
     }, 500);
-    return false;
+    choices.veggie = true;
+    choices.meat = true;
+    choices.meats = ['beef', 'chicken', 'fish', 'pork'];
+    choices.weight = 'either';
+    choices.time = 'quickAndSlow';
+    console.log(choices);
   })
 
   $('#picky').on('click', function() {
     $($wholeThing).animate({
         scrollTop: $('#step2').offset().top
     }, 500);
-    return false;
   })
 //step2
   $('#veggie').on('click', function() {
     $($wholeThing).animate({
         scrollTop: $('#step4').offset().top
     }, 500);
-    return false;
+    choices.veggie = true;
+    choices.meat   = false;
+    choices.meats = null;
+    console.log(choices);
   })
 
   $('#meat').on('click', function() {
     $($wholeThing).animate({
         scrollTop: $('#step3').offset().top
     }, 500);
-    return false;
+    choices.veggie = false;
+    choices.meat = true;
+    console.log(choices);
   })
 //step3
   $('.meats').on('click', function() {
-
+//not sure i need this yet
   })
 
   $('#beef').on('click', function() {
-
+    choices.meats.push('beef');
+    console.log(choices);
   })
 
   $('#chicken').on('click', function() {
-
+    choices.meats.push('chicken');
+    console.log(choices);
   })
 
   $('#fish').on('click', function() {
-
+    choices.meats.push('fish');
+    console.log(choices);
   })
 
   $('#pork').on('click', function() {
-
+    choices.meats.push('pork');
+    console.log(choices);
   })
-//if all 4 pushed, same as pushing go
+
   $('#onward').on('click', function() {
     $($wholeThing).animate({
         scrollTop: $('#step4').offset().top
     }, 500);
-    return false;
   })
 //step4
   $('#light').on('click', function() {
     $($wholeThing).animate({
         scrollTop: $('#step5').offset().top
     }, 500);
-    return false;
+    choices.weight = 'light';
   })
 
   $('#comfort').on('click', function() {
     $($wholeThing).animate({
         scrollTop: $('#step5').offset().top
     }, 500);
-    return false;
+    choices.weight = 'comfort';
   })
 
   $('#non').on('click', function() {
     $($wholeThing).animate({
         scrollTop: $('#step5').offset().top
     }, 500);
-    return false;
+    choices.weight = 'either';
   })
 //step5
   $('#quick').on('click', function() {
     $($wholeThing).animate({
         scrollTop: $('#step6').offset().top
     }, 500);
-    return false;
+    choices.time = 'quick';
   })
 
   $('#slow').on('click', function() {
     $($wholeThing).animate({
         scrollTop: $('#step6').offset().top
     }, 500);
-    return false;
+    choices.time = 'slow';
   })
 
   $('#slowCooker').on('click', function() {
     $($wholeThing).animate({
         scrollTop: $('#step6').offset().top
     }, 500);
-    return false;
+    choices.time = 'slowCooker';
   })
 //step6
   $('#yes').on('click', function() {
-
+    choices.side = true;
+    console.log(choices);
   })
 
   $('#no').on('click', function() {
-
+    choices.side = false;
+    console.log(choices);
   })
 //submit
   $('#submit').on('click', function() {
     $($wholeThing).animate({
         scrollTop: $('#results').offset().top
     }, 500);
-    return false;
+//run UserInput on var choices
   })
+
+
+
+
+
+
+
+
+
+
+
 
 
 });

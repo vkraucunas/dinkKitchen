a763c7c659de19f9e988e027da29dd3a49fcda32
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-02-04 10:54:13 -0700

    all the stuff, i think

---
+++ src/js/main.js
@@ -0,0 +1,55 @@
+var entreeCards;
+var temp;
+var sideCards;
+
+
+
+$(document).on('ready', function() {
+
+  $('#entreeRecipe').hide();
+  $('#sideRecipe').hide();
+
+
+
+var getFunction = function(url, key) {
+  return new Promise(function (resolve, reject) {
+    $.get(url).done(function (response) {
+        resolve(response[key]);
+      });
+  });
+};
+
+getFunction('http//jsonip.com', 'ip').then(function(ip) {
+  var IP = ip;
+  var newURL = "//freegeoip.net/json/"+IP;
+  return getFunction(newURL, 'city');
+}).then(function(city) {
+  var City = city;
+  var lastURL = '//api.openweathermap.org/data/2.5/weather?q='+City+'&units=imperial&APPID=f708ff06c29d9c50b54499dfec6a5a05';
+  return getFunction(lastURL, 'main').then(function (main) {
+    temp = Math.floor(main.temp);
+    console.log("Temp variable", temp);
+  });
+});
+
+
+
+  var Trello = $.ajax({
+    url: "//api.trello.com/1/boards/56a997d37317034310b39031/lists?cards=open&card_fields=all&fields=name&key=c08e97c955fd88aba5f367ed9c47c0d4&token=9767918595f60e414fe3c1db21b106abc15fe9f3228609b9e68c265e12b9595c",
+    method: "GET",
+    success: function(data) {
+      var entreeList = data[0];
+      var sideList = data[1];
+      entreeCards = entreeList.cards;
+
+      entreeCardLabels = entreeCards.labels;
+      sideCards = sideList.cards;
+      console.log(entreeCards);
+      console.log(sideCards);
+    }
+  });
+
+
+
+//document.ready closer. no touchy.
+});
